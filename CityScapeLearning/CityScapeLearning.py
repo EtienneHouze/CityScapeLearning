ed714a36d3eef7c08a604d505194012ee0370895
Etienne.Houze <Etienne.Houze@EMEA20708>
2017-03-28 18:04:18 +0200

    Added comments and the skeleton of a loss function

--- CityScapeLearning/CityScapeLearning.py
+++ CityScapeLearning/CityScapeLearning.py
@@ -9,10 +9,15 @@ from helpers import *
 
 batch_size = 10
 
+def loss(net):
+    return 0
+
+
 with tf.Graph().as_default():
-    test_input = tf.placeholder(shape=(batch_size,10),dtype=tf.float32)
+    test_input = tf.placeholder(shape=(batch_size,1920,1080,3),dtype=tf.float32)
     test = Network(test_input)
-    test.add_FCLayer(layer_size = [1024])
-    test.add_FCLayer(layer_size = [1024,16])
+    test.add_FCLayer(layer_size = [512,512,16])
+    #test.add_FCLayer(layer_size = [1024,16])
+    test.add_conv_Layer(kernel_size=[3,3],padding="SAME",stride=[1,1,1,1],out_depth=32)
     a=0
 
--- CityScapeLearning/Network.py
+++ CityScapeLearning/Network.py
@@ -9,9 +9,20 @@ import numpy as np
 """
 class Network:
      
+    """
+        Initialization of the network.
+        Args :
+            - input : a tensor of the inputs of the network
+        Properties :
+            - input : the input tensor
+            - batch_size : a scalar, the first dimension of the input tensor which is the size of a batch used in the learning phase
+            - layers : a list of tensors representing the successive layers of neurons of the net. layers[0] is the input.
+            - variables : a list of tf.Variables which are to be tuned in the learning phase.
+            - number_of_layers : self-explanatory...
+    """
     def __init__(self,input):
         self.input = input
-        self.batch_size = input._shape.as_list()[0]
+        self.batch_size = input._shape[0].value
         self.layers = [self.input]
         self.variables = []
         self.number_of_layers = 0
@@ -37,9 +48,24 @@ class Network:
             self.layers.append(h)
         self.number_of_layers += 1
 
-    def add_conv_Layer(self,kernel_size,stride,padding):
+    def add_conv_Layer(self,kernel_size,stride,padding,out_depth):
         with tf.name_scope('conv_'+str(self.number_of_layers)):
-            print('haha')
+            in_depth = self.layers[-1]._shape[-1].value
+            F = tf.Variable(initial_value=tf.random_normal(shape=kernel_size+[in_depth]+[out_depth]),
+                            dtype=tf.float32,
+                            name = 'Filter_conv_'+str(self.number_of_layers))
+            b = tf.Variable(initial_value=tf.zeros(shape=self.layers[-1]._shape.as_list()[:-1]+[out_depth]),
+                            dtype = tf.float32,
+                            name = 'Bias_conv_'+str(self.number_of_layers))
+            h = tf.nn.relu( tf.nn.conv2d(input = self.layers[-1],
+                                         filter=F,
+                                         strides=stride,
+                                         padding=padding)+b,
+                           name = 'Conv_'+str(self.number_of_layers))
+            self.variables.append(F)
+            self.variables.append(b)
+            self.layers.append(h)
+        self.number_of_layers += 1
 
     def compute_output(self):
         with tf.name_scope('Output'):
